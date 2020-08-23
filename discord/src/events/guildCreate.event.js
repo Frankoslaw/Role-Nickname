91fde6dad52de48f0b4b58b336c4a9c3eec59019
FranciszekLopuszanski <franciszeklopuszanski@MacBook-Pro-Sawomir.local>
2020-08-23 22:26:29 +0200

    clear git cache to update git ignore

--- discord/src/events/guildCreate.event.js
+++
@@ -1,9 +0,0 @@
-const { debug_mode } = require(__dirname + '/../configs/config.js');
-const { saveConfig } = require('../handlers/Config.handler.js')
-
-module.exports = {
-    name: 'guildCreate',
-    execute(guild) {
-        saveConfig(guild.id, {"RolesPrefix": {},"MandatoryPrefix": true,"NumberOfPrefixs": 1})
-    }
-}
