2b6b4bca10573267c693abc4cb9914b9d063bd36
FranciszekLopuszanski <franciszeklopuszanski@MacBook-Pro-Sawomir.local>
2020-08-24 14:07:22 +0200

    gitignore update

--- discord/src/events/guildDelete.event.js
+++
@@ -1,9 +0,0 @@
-const { debug_mode } = require(__dirname + '/../configs/config.js');
-const { deleteConfig } = require('../handlers/Config.handler.js')
-
-module.exports = {
-    name: 'guildDelete',
-    execute(guild) {
-        deleteConfig(guild.id)
-    }
-}
